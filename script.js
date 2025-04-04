398ad6f6f2a0388b7ef98c39e4b60a06efd657ff
Matheus (M6) <133786715+Matheus15CAP@users.noreply.github.com>
2025-04-04 07:45:09 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
