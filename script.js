a3f2e93c77b26fb7970782daf7c1f592829bbb61
Matheus (M6) <133786715+Matheus15CAP@users.noreply.github.com>
2025-04-06 00:49:12 -0300

    .

---
+++ script.js
@@ -0,0 +1,50 @@
+document.getElementById('btn-acessibilidade').addEventListener('click', function() {
+    const botoesAcessibilidade = document.getElementById('botoes-acessibilidade');
+    botoesAcessibilidade.style.display = botoesAcessibilidade.style.display === 'none' ? 'block' : 'none';
+});
+
+let fontSize = 20; 
+const fontSizeMin = 16; 
+const fontSizeMax = 26; 
+
+
+function atualizarFonte() {
+    document.body.style.fontSize = fontSize + 'px';
+}
+
+// Aumentar a fonte
+document.getElementById('btn-aumentar').addEventListener('click', function() {
+    if (fontSize < fontSizeMax) {
+        fontSize += 2; 
+        atualizarFonte(); 
+    }
+});
+
+// Diminuir a fonte
+document.getElementById('btn-diminuir').addEventListener('click', function() {
+    if (fontSize > fontSizeMin) {
+        fontSize -= 2; 
+        atualizarFonte(); 
+    }
+});
+
+atualizarFonte();
+
+let textoBranco = false;
+
+document.getElementById('btn-mudar-cor').addEventListener('click', function() {
+    
+    textoBranco = !textoBranco; 
+
+    if (textoBranco) {
+        
+        document.querySelectorAll('body *:not(#contato):not(#contato *)').forEach(function(el) {
+            el.style.color = '';
+        });
+    } else {
+        
+        document.querySelectorAll('body *:not(#contato):not(#contato *)').forEach(function(el) {
+            el.style.color = 'white'; 
+        });
+    }
+});
