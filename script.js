6fe04cb004efac66fd354ef82967ed2eea5be8ef
Matheus (M6) <133786715+Matheus15CAP@users.noreply.github.com>
2025-04-25 08:00:04 -0300

    .

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('btn-acessibilidade').addEventListener('click', function
 
 let fontSize = 20; 
 const fontSizeMin = 16; 
-const fontSizeMax = 28; 
+const fontSizeMax = 27; 
 
 
 function atualizarFonte() {
